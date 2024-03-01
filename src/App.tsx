7adb43953d84d600744eb03a8977be4254b2e749
Sugiyama Natsuki <natsur990704@gmail.com>
2024-03-01 17:28:15 +0900

    Displaying avatars and key controlls

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Avatar } from "@/components/basics/Avatar/Avatar";
+import { defaultAnimationMap } from "@/components/basics/Avatar/Avatar.function";
+import usePlayerInput from "@/hooks/usePlayerInput";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const playerInput = usePlayerInput();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div style={{ width: "80vw", height: "80vh" }}>
+          <Canvas linear={true} flat={true}>
+            <OrbitControls />
+            <ambientLight intensity={7} />
+            <Avatar
+              avatarPath={"public/avatars/timmy.vrm"}
+              controller={playerInput.movement}
+              animationMap={defaultAnimationMap}
+            ></Avatar>
+          </Canvas>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
