12412fcc9cebd01cefca014175e3bf88acd6b7b3
Sugiyama Natsuki <natsur990704@gmail.com>
2024-03-04 12:04:59 +0900

    Delete unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,23 @@ import { defaultAnimationMap } from "@/components/basics/Avatar/Avatar.function"
 import usePlayerInput from "@/hooks/usePlayerInput";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import "./App.css";
 
 function App() {
   const playerInput = usePlayerInput();
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div style={{ width: "80vw", height: "80vh" }}>
-          <Canvas linear={true} flat={true}>
-            <OrbitControls />
-            <ambientLight intensity={7} />
-            <Avatar
-              avatarPath={"public/avatars/timmy.vrm"}
-              controller={playerInput.movement}
-              animationMap={defaultAnimationMap}
-            ></Avatar>
-          </Canvas>
-        </div>
-      </main>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <Canvas linear={true} flat={true}>
+          <OrbitControls />
+          <ambientLight intensity={7} />
+          <Avatar
+            avatarPath={"public/avatars/timmy.vrm"}
+            controller={playerInput.movement}
+            animationMap={defaultAnimationMap}
+          ></Avatar>
+        </Canvas>
+      </div>
     </>
   );
 }
