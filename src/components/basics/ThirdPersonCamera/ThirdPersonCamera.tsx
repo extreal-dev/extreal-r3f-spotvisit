af33e56dd835bf845229fbd1c0970ed84c30b216
Sugiyama Natsuki <natsur990704@gmail.com>
2024-05-10 16:27:22 +0900

    Disable camera control when player is moving

--- src/components/basics/ThirdPersonCamera/ThirdPersonCamera.tsx
+++ src/components/basics/ThirdPersonCamera/ThirdPersonCamera.tsx
@@ -20,6 +20,7 @@ export type ThirdPersonCameraProps = {
   cameraOffset?: THREE.Vector3;
   targetOffset?: THREE.Vector3;
   enableZoomAndPan?: boolean;
+  enableControl?: boolean;
 };
 
 // ThirdPerson Camera based on OrbitControls for Avatar Control
@@ -27,7 +28,6 @@ const ThirdPersonCamera = (props: ThirdPersonCameraProps) => {
   // Drei currently does not forward Ref of OrbitControls(https://github.com/pmndrs/drei/issues/937)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const orbitControlsRef = useRef<any>(null);
-  const lastUpdateTime = useRef(performance.now());
   const { movement, avatarRef } = props;
   const initialCameraOffset = useMemo(
     () => props.cameraOffset ?? new THREE.Vector3(0, 2, -5),
@@ -42,35 +42,37 @@ const ThirdPersonCamera = (props: ThirdPersonCameraProps) => {
   );
   const [cameraMove, setCameraMove] = useState(false);
   const enableZoomAndPan = props.enableZoomAndPan ?? true;
+  const [savedPolarAngle, setSavedPolarAngle] = useState(Math.PI * 0.5);
+  const [savedAzimuthAngle, setSavedAzimuthAngle] = useState(Math.PI * 0.5);
 
   const { camera } = useThree();
 
   // Initiate Camera Position by Avatar Load and changes
   useEffect(() => {
     if (avatarRef.current) {
-      const controls = orbitControlsRef.current as OrbitControlsProps;
-      if (controls.object) {
-        controls.object.position.copy(initialCameraOffset);
+      const orbitControls = orbitControlsRef.current as OrbitControlsProps;
+      if (orbitControls.object) {
+        orbitControls.object.position.copy(initialCameraOffset);
       }
     }
   }, [initialCameraOffset, avatarRef]);
 
+  const isPlayerMoving =
+    movement.forward ||
+    movement.backward ||
+    movement.left ||
+    movement.right ||
+    movement.running;
   useFrame(() => {
     const controls = orbitControlsRef.current as OrbitControlsProps;
     const avatarPos = avatarRef.current?.getPosition();
     if (avatarPos && controls.target && controls.update && controls.object) {
       //Set camera target
       if (controls.target instanceof THREE.Vector3) {
-        controls.target.copy(avatarPos.clone().add(targetOffset));
+        controls.target.copy(avatarPos).add(targetOffset);
       }
       //Camera keeps constant distance from Avatar
-      if (
-        movement.forward ||
-        movement.backward ||
-        movement.left ||
-        movement.right ||
-        movement.running
-      ) {
+      if (isPlayerMoving) {
         //Direction from current camera position to Avatar
         const currentCameraPos = controls.object.position.clone();
         if (currentCameraPos) {
@@ -80,30 +82,27 @@ const ThirdPersonCamera = (props: ThirdPersonCameraProps) => {
           const updatedOffset = directionVector.multiplyScalar(distance);
 
           //Update camera position with updated offset
-          const updatedCameraPos = avatarPos.clone().add(updatedOffset);
-          controls.object.position.copy(updatedCameraPos);
+          controls.object.position.copy(avatarPos).add(updatedOffset);
         }
       }
       controls.update();
 
       //Keep distance even after camera zooming or panning
-      setDistance(controls.object.position.clone().distanceTo(avatarPos));
-    }
-  });
+      setDistance(controls.object.position.distanceTo(avatarPos));
 
-  // Throttle the camera position settings
-  // 3000ms interval avoids excessive requests and ensures accuracy
-  const interval = 3000;
-  useFrame(() => {
-    const now = performance.now();
-    if (now - lastUpdateTime.current >= interval) {
-      if (cameraMove) {
-        props.setMovement((state) => ({
-          ...state,
-          cameraPosition: camera.position,
-        }));
+      if (controls.getPolarAngle) {
+        setSavedPolarAngle(controls.getPolarAngle());
       }
-      lastUpdateTime.current = now;
+      if (controls.getAzimuthalAngle) {
+        setSavedAzimuthAngle(controls.getAzimuthalAngle());
+      }
+    }
+
+    if (cameraMove) {
+      props.setMovement((state) => ({
+        ...state,
+        cameraPosition: camera.position,
+      }));
     }
   });
 
@@ -114,10 +113,15 @@ const ThirdPersonCamera = (props: ThirdPersonCameraProps) => {
         onEnd={() => setCameraMove(false)}
         ref={orbitControlsRef}
         makeDefault
+        // Disable camera control when player is moving to prevent camera shaking
+        enabled={!isPlayerMoving}
         // Prevent Gimbal lock by limiting polar angles
         // 0.1 to 0.9 is ideal for free camera control
-        minPolarAngle={Math.PI * 0.1}
-        maxPolarAngle={Math.PI * 0.9}
+        // Lock camera angles for intuitive control when player is moving,
+        minPolarAngle={isPlayerMoving ? savedPolarAngle : Math.PI * 0.1}
+        maxPolarAngle={isPlayerMoving ? savedPolarAngle : Math.PI * 0.9}
+        minAzimuthAngle={isPlayerMoving ? savedAzimuthAngle : undefined}
+        maxAzimuthAngle={isPlayerMoving ? savedAzimuthAngle : undefined}
         // Limit camera distance so that it does not enter inside Avatar and is not too far away
         // 2.0 to 10.0 is ideal for both PC and mobile devices
         minDistance={2.0}
